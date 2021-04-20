d8e1446eacd3bd8a6ccf2f27209b38cd3276ffef
Ivan Annovazzi <ivan@modusbox.com>
2021-04-20 18:15:22 +0200

    Remove unnecessary config (#4)

--- jest.config.js
+++ jest.config.js
@@ -3,20 +3,7 @@ module.exports = {
   //   './test/jestsetup.js'
   // ],
   'testURL': 'http://localhost/',
-  'moduleNameMapper': {
-    'ansi-styles': '<rootDir>/node_modules/ansi-styles',
-    'jest-matcher-utils': '<rootDir>/node_modules/jest-matcher-utils',
-    '^react-(.+)': '<rootDir>/node_modules/react-$1',
-    '^react-dom': '<rootDir>/node_modules/react-dom',
-    '^react[/](.+)': '<rootDir>/node_modules/react/$1',
-    '^react': '<rootDir>/node_modules/react',
-    '^views': '<rootDir>/src/react/views',
-    '^icons': '<rootDir>/src/react/icons',
-    '^components': '<rootDir>/src/react/components',
-    '^common[/](.+)': '<rootDir>/src/common/$1',
-    '^test[/](.+)': '<rootDir>/test/$1',
-    '^.+\\.(css|png|scss|svg)$': '<rootDir>/src/react/test/__mocks__/staticMocks.js'
-  },
+  'moduleNameMapper': {},
   'moduleFileExtensions': [
     'ts',
     'js',
