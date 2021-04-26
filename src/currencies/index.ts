50d3a8514278ef30cd9d7618d15585f48e048b0e
Ivan Annovazzi <ivan@modusbox.com>
2021-04-26 15:00:18 +0200

    Currencies modules (#13)
    * Currencies modules
    
    * v0.0.2-4
    
    * Export currencies

---
+++ src/currencies/index.ts
@@ -0,0 +1,3 @@
+export { default as formatAmount } from './formatAmount';
+export { default as findCurrencySymbol } from './findCurrencySymbol';
+export * from './types';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as async from './async';
 import * as file from './file';
+import * as currencies from './currencies';
 import * as localstorage from './localstorage';
 import * as html from './html';
 import * as http from './http';
 import * as testers from './testers';
 import * as validation from './validation';
 
-export { async, file, localstorage, html, http, testers, validation };
+export { async, file, currencies, localstorage, html, http, testers, validation };
