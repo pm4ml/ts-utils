a4accc75965de5186441049200e8f44d7c43aed8
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-21 15:18:43 +0200

    Export readFileAsBase64

--- src/file/index.ts
+++ src/file/index.ts
@@ -1,5 +1,6 @@
 export { default as downloadFile } from './downloadFile';
 export { default as readFileAsText } from './readFileAsText';
+export { default as readFileAsBase64 } from './readFileAsBase64';
 export { default as loadFile } from './loadFile';
 export { default as copyToClipboard } from './copyToClipboard';
 export * from './types';
