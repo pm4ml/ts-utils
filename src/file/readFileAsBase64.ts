eb1a7070427b110e3a67ea63725c5a44de73b380
Ivan Annovazzi <ivan@modusbox.com>
2021-05-21 15:20:56 +0200

    Base64file (#14)
    * Export readFileAsBase64
    
    * v0.0.2-5

---
+++ src/file/readFileAsBase64.ts
@@ -0,0 +1,12 @@
+import { TextFileContent } from './types';
+
+function readFileAsBase64(file: File): Promise<TextFileContent> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => resolve(event?.target?.result);
+    reader.onerror = (error) => reject(error);
+    reader.readAsDataURL(file);
+  });
+}
+
+export default readFileAsBase64;
